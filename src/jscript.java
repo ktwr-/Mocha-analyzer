b5a4c76572ee323440c9f038e834b3eafbe21931
takeuchi <kataware8136@gmail.com>
2015-09-08 16:41:15 +0900

    correspondence of only inlinescript
    this progress, I can divide inline script from html file.
    With the actual situation, I don't write inlinescript at another file.

--- src/jscript.java
+++ src/jscript.java
@@ -11,21 +11,28 @@ import java.util.regex.Pattern;
 public class jscript {
 	
 	static ArrayList<String> removetag = new ArrayList<String>();
+	static ArrayList<String> fileextension = new ArrayList<String>();
+	static char jsfilename = 'a';
+	static char stylefilename = 'a';
 	
 	public static void main(String args[]){
 		init();
 		//System.out.println(args[0]);
-		findCSP("test");
+		analyzehtml("test");
+
 	}
 	public static void init(){
 		removetag.add("script");
 		removetag.add("style");
 		removetag.add("eval");
+		fileextension.add(".js");
+		fileextension.add("eval");
+		fileextension.add("css");
 		
 	}
 	
 	/*catch program css, script or anything else which CSP applying*/
-	public static void findCSP(String filename){
+	public static void analyzehtml(String filename){
 		try{
 			File file = new File("./test/test.html");
 			FileReader filereader = new FileReader(file);
@@ -33,35 +40,41 @@ public class jscript {
 			/*flag is which part is the match word,script,eval or style*/
 			int i;
 			String str,pat1,pat2="";
+			String div="";
 			while((str = bf.readLine()) != null){
 			
 				/*find tag*/
 				for(i =0;i < removetag.size();i++){
 					/*find script,eval or style tag*/
-					pat1 = "(.*)(<"+removetag.get(i)+">.*)";
+					pat1 = "(.*)(<"+removetag.get(i)+".*)";
 					pat2 = "(.*</"+removetag.get(i)+">)(.*)";
 					if(str.matches(pat1)){
 						Pattern p = Pattern.compile(pat1);
 						Matcher m = p.matcher(str);
 						if(m.find()){
 							// take word after <script>
-							System.out.println(m.group(2));
+							//System.out.println(m.group(2));
+							div = m.group(2)+"\n";
 						}
 						break;
 					}
 				}
+				
 				/*write htmlfile for script*/
 				switch(i){
 				case 0:
 					while(!(str = bf.readLine()).matches(pat2)){
-						System.out.println(str);
+						//System.out.println(str);
+						div += str+"\n";
 					}
 					Pattern p1 = Pattern.compile(pat2);
 					Matcher m1 = p1.matcher(str);
 					if(m1.find()){
-						System.out.println(m1.group(1)+"\nendscript");
+						//System.out.println(m1.group(1)+"\n");
+						div += m1.group(1);
 					}
-					
+					dividefile(div,i);
+					break;
 				default:
 					System.out.println(str);
 					break;
@@ -77,10 +90,15 @@ public class jscript {
 		
 	}
 	
-	/*divide javascript from html*/
-	public static void writescript(String text){
+	/*divide file from html*/
+	public static void dividefile(String text,int type){
+		//System.out.println(text);
+		/*write file*/
 		
+		/*instead of divide, write like <script src="~~" ></script> text*/
 		
+		System.out.println("<"+removetag.get(type)+" src=\""+jsfilename+fileextension.get(type)+"\"></"+removetag.get(type)+">\n");
+		jsfilename++;
 	}
 	
 	/*divide CSS from html*/
